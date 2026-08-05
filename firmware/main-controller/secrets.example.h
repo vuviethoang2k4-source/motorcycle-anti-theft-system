a8988fb2fbe149047da819e0e8bd49318abc900c
vuviethoang2k4-source <vuviethoang2k4@gmail.com>
2026-08-05 10:56:53 +0700

    Add example configuration files

---
+++ firmware/main-controller/secrets.example.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_CONTROLLER_SECRETS_EXAMPLE_H
+#define MAIN_CONTROLLER_SECRETS_EXAMPLE_H
+
+// Số điện thoại chủ xe được phép điều khiển qua SMS
+#define OWNER_PHONE_NUMBER "+84869915241"
+
+// Địa chỉ MAC của remote ESP32-C3
+const uint8_t REMOTE_MAC_ADDRESS[6] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+#endif
